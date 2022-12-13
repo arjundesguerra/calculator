46160eb60e18a2c16337c0cd135a779fbe42076f
arjundesguerra <arjundesguerra@gmail.com>
2022-12-12 23:05:06 -0800

    functional calculator

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
 const now = document.getElementById('now');
 const previous = document.getElementById('previous');
+
+let operatorInput = false; //prevents double operation input
+let previousNum;
+let nowNum;
+
 function input(event) {
     validNumbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
-    if (event.target.id in validNumbers) {
+    if (event.target.id in validNumbers || event.target.id === '.') {
         now.textContent += event.target.id;
+        operatorInput = false;
     }
-    if (event.target.id === '+' || event.target.id === '-' ||
-        event.target.id === 'x' || event.target.id === '÷') {
+    if (event.target.id === '+' && operatorInput == false || event.target.id === '-' && operatorInput == false ||
+        event.target.id === '*' && operatorInput == false || event.target.id === '/' && operatorInput == false) {
         previous.textContent += `${now.textContent} ${event.target.id} `
+        previousNum = previous.textContent;
         now.textContent = "";
+        nowNum = now.textContent;
+        operatorInput = true;
     }
     if (event.target.id === 'clear') {
         now.textContent = "";
+        previous.textContent = "";
     }
     if (event.target.id === 'delete') {
         now.textContent = now.textContent.slice(0, -1);
     }
-}
-
-
-
-// math functions
-function add(num1, num2) {
-    return num1 + num2;
-}
-
-function subtract(num1, num2) {
-    return num1 - num2;
-}
-
-function multiply(num1, num2) {
-    return num1 * num2;
-}
-
-function divide(num1, num2) {
-    return num1 / num2;
-}
+    if (event.target.id === '=') {
+        previous.textContent += now.textContent;
+        now.textContent = eval(previous.textContent);
+    }
+}   
